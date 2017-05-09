fe5db2f1cda41f8d5a71f20c5ad3479d4a0c8aa8
Bas de Vos <bas.de.vos@42.nl>
2017-05-09 14:34:45 +0200

    Added an Order of 0 to the WebMvcErrorHandler to be executed before default ordered controllerAdvice beans.

--- src/main/java/nl/_42/restsecure/autoconfigure/components/WebMvcErrorHandler.java
+++ src/main/java/nl/_42/restsecure/autoconfigure/components/WebMvcErrorHandler.java
@@ -1,14 +1,14 @@
 package nl._42.restsecure.autoconfigure.components;
 
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
-@ResponseBody
+@RestControllerAdvice
+@Order(0)
 public class WebMvcErrorHandler {
 
     public static final String SERVER_ACCESS_DENIED_ERROR = "SERVER.ACCESS_DENIED_ERROR";
@@ -18,4 +18,5 @@ public class WebMvcErrorHandler {
     public GenericErrorResult handlesAccessDeniedException() {
         return new GenericErrorResult(SERVER_ACCESS_DENIED_ERROR);
     }
+
 }
