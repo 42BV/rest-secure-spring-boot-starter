6e278838cc3983654caf2744a28e8b64197347ac
Bas de Vos <bas.de.vos@42.nl>
2017-12-13 10:48:27 +0100

    Added access denied test for valid session

--- src/test/java/nl/_42/restsecure/autoconfigure/AccessDeniedHandlerTest.java
+++ src/test/java/nl/_42/restsecure/autoconfigure/AccessDeniedHandlerTest.java
@@ -1,12 +1,24 @@
 package nl._42.restsecure.autoconfigure;
 
+import static nl._42.restsecure.autoconfigure.RestAccessDeniedHandler.SERVER_AUTHENTICATE_ERROR;
+import static nl._42.restsecure.autoconfigure.RestAccessDeniedHandler.SERVER_SESSION_INVALID_ERROR;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+import javax.servlet.ServletContext;
 
 import nl._42.restsecure.autoconfigure.shared.test.AbstractApplicationContextTest;
 import nl._42.restsecure.autoconfigure.shared.test.config.RestrictedEndpointsConfig;
+import nl._42.restsecure.autoconfigure.shared.test.config.UserDetailsServiceConfig;
 
 import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 public class AccessDeniedHandlerTest extends AbstractApplicationContextTest {
 
@@ -16,5 +28,48 @@ public class AccessDeniedHandlerTest extends AbstractApplicationContextTest {
             .perform(get("/test/forbidden"))
             .andExpect(status().isForbidden());
     }
+    
+    @Test
+    public void currentUser_shouldFail_withInvalidSession_whenNotLoggedIn() throws Exception {
+        loadApplicationContext(UserDetailsServiceConfig.class);
+        webAppContextSetup(context)
+            .apply(springSecurity())
+            .build()
+            .perform(new RequestBuilder() {
+                @Override
+                public MockHttpServletRequest buildRequest(ServletContext servletContext) {
+                    MockHttpServletRequest request = MockMvcRequestBuilders
+                            .get("/authentication/current")
+                            .buildRequest(servletContext);
+                    request.setRequestedSessionId("sessionid");
+                    request.setRequestedSessionIdValid(false);
+                    return request;
+                }
+            })
+            .andDo(print())
+            .andExpect(status().isUnauthorized())
+            .andExpect(jsonPath("errorCode").value(SERVER_SESSION_INVALID_ERROR));
+    }
 
+    @Test
+    public void currentUser_shouldFail_withValidSession_whenNotLoggedIn() throws Exception {
+        loadApplicationContext(UserDetailsServiceConfig.class);
+        webAppContextSetup(context)
+            .apply(springSecurity())
+            .build()
+            .perform(new RequestBuilder() {
+                @Override
+                public MockHttpServletRequest buildRequest(ServletContext servletContext) {
+                    MockHttpServletRequest request = MockMvcRequestBuilders
+                            .get("/authentication/current")
+                            .buildRequest(servletContext);
+                    request.setRequestedSessionId("sessionid");
+                    request.setRequestedSessionIdValid(true);
+                    return request;
+                }
+            })
+            .andDo(print())
+            .andExpect(status().isUnauthorized())
+            .andExpect(jsonPath("errorCode").value(SERVER_AUTHENTICATE_ERROR));
+    }
 }
