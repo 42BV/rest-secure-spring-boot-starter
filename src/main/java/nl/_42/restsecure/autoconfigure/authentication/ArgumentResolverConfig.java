122e1ca26a0bf24795f3c3cf8280531814a82225
Bas de Vos <bas.de.vos@42.nl>
2020-11-23 12:08:44 +0100

    Made UserResolver in ArgumentResolverConfig lazy to avoid circular dependency errors.

--- src/main/java/nl/_42/restsecure/autoconfigure/authentication/ArgumentResolverConfig.java
+++ src/main/java/nl/_42/restsecure/autoconfigure/authentication/ArgumentResolverConfig.java
@@ -2,17 +2,19 @@ package nl._42.restsecure.autoconfigure.authentication;
 
 import java.util.List;
 
-import lombok.RequiredArgsConstructor;
-
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@RequiredArgsConstructor
 public class ArgumentResolverConfig implements WebMvcConfigurer {
 
     private final UserResolver<RegisteredUser> userResolver;
+
+    public ArgumentResolverConfig(@Lazy UserResolver<RegisteredUser> userResolver) {
+        this.userResolver = userResolver;
+    }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
