9695cb0b2f950871c75cf32d1dae4866b1d38fee
Jeroen van Schagen <jeroen.van.schagen@42.nl>
2021-04-21 16:33:58 +0200

    OWASP fixes, upgrade to spring boot 2.4.5 and remove commons-io

--- src/main/java/nl/_42/restsecure/autoconfigure/RestAuthenticationFilter.java
+++ src/main/java/nl/_42/restsecure/autoconfigure/RestAuthenticationFilter.java
@@ -1,20 +1,9 @@
 package nl._42.restsecure.autoconfigure;
 
-import static org.springframework.http.HttpMethod.POST;
-
-import java.io.IOException;
-import java.util.Optional;
-
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import nl._42.restsecure.autoconfigure.authentication.AbstractRestAuthenticationSuccessHandler;
 import nl._42.restsecure.autoconfigure.errorhandling.LoginAuthenticationExceptionHandler;
-
-import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -26,8 +15,16 @@ import org.springframework.security.web.authentication.RememberMeServices;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Optional;
+
+import static org.springframework.http.HttpMethod.POST;
 
 /**
  * Handles the login POST request. Tries to Authenticate the given user credentials using the auto configured {@link AuthenticationManager}.
@@ -74,7 +71,7 @@ public class RestAuthenticationFilter extends OncePerRequestFilter {
     }
 
     private void doLogin(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
-        String loginFormJson = IOUtils.toString(request.getReader());
+        String loginFormJson = read(request.getReader());
         LoginForm form = objectMapper.readValue(loginFormJson, LoginForm.class);
         UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(form.username, form.password);
 
@@ -93,6 +90,22 @@ public class RestAuthenticationFilter extends OncePerRequestFilter {
             chain.doFilter(request, response);
         } catch (AuthenticationException ex) {
             handleLoginFailure(request, response, ex);
+        }
+    }
+
+    private String read(Reader reader) {
+        try {
+            StringBuilder builder = new StringBuilder();
+
+            int result;
+            while ((result = reader.read()) != -1) {
+                builder.append((char) result);
+            }
+
+            reader.close();
+            return builder.toString();
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Could not use reader", ioe);
         }
     }
 
