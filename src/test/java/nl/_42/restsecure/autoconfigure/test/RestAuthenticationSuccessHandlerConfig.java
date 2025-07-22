b17165f454fbb40ac9d3f08e488c89c70870ba94
Thijs Mesman <thijs.mesman@42.nl>
2025-07-22 14:10:49 +0200

    bugfix: fix broken testsetup

--- src/test/java/nl/_42/restsecure/autoconfigure/test/RestAuthenticationSuccessHandlerConfig.java
+++ src/test/java/nl/_42/restsecure/autoconfigure/test/RestAuthenticationSuccessHandlerConfig.java
@@ -2,22 +2,28 @@ package nl._42.restsecure.autoconfigure.test;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import nl._42.restsecure.autoconfigure.authentication.AbstractRestAuthenticationSuccessHandler;
 import nl._42.restsecure.autoconfigure.authentication.RegisteredUser;
+import nl._42.restsecure.autoconfigure.authentication.UserResolver;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
-public class RestAuthenticationSuccessHandlerConfig {
+public class RestAuthenticationSuccessHandlerConfig<T extends RegisteredUser> {
 
     public static final String AUTHENTICATION_SUCCESS_HEADER = "AuthenticationSuccessful";
 
+    private final UserResolver<T> userResolver;
+
+    public RestAuthenticationSuccessHandlerConfig(UserResolver<T> userResolver) {
+        this.userResolver = userResolver;
+    }
+
     @Bean
-    public <T extends RegisteredUser> AbstractRestAuthenticationSuccessHandler<T> successHandler() {
-        return new AbstractRestAuthenticationSuccessHandler<>(null) {
+    public AbstractRestAuthenticationSuccessHandler<T> successHandler() {
+        return new AbstractRestAuthenticationSuccessHandler<>(userResolver) {
             @Override
             protected void doHandle(HttpServletRequest request, HttpServletResponse response, RegisteredUser user) {
                 response.setHeader(AUTHENTICATION_SUCCESS_HEADER, SecurityContextHolder.getContext().getAuthentication().getName());
